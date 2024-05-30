a2b5c2b4b62f174c37455e5c539073007f067668
Yansiqu <166083851+Yansiqu@users.noreply.github.com>
2024-05-29 23:29:34 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    #imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+
 
     def __str__(self):
         return self.nome
@@ -13,6 +17,30 @@ class Produto(models.Model):
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
+    descricao = models.TextField()
+    telefone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.nome
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O email é obrigatório.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
