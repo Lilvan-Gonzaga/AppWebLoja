61d9d4a3b7a193c7d81cc9cfefe8c6e2f2e6b7ef
LILVAN GONZAGA <lilvangonzaga@gmail.com>
2024-06-02 00:03:46 -0300

    estrutura inicial pronta

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
+
+def lista_produtos(request):
+    produtos = Produto.objects.filter(disponivel=True)
+    return render(request, 'core/index.html')
