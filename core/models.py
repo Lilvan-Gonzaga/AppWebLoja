e843ab7dab027fc1ffb28956b235398e7dbb03ca
LILVAN GONZAGA <lilvangonzaga@gmail.com>
2024-06-05 22:02:15 -0300

    commit 7

--- core/models.py
+++ core/models.py
@@ -17,7 +17,7 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     disponivel = models.BooleanField(default=True)
-    #imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+    imagem = models.ImageField(upload_to='media/', blank=True, null=True)
 
     def __str__(self):
         return self.nome
--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Produto
+from .models import Produto, UserManager
 
 # Create your views here.
 
 def lista_produtos(request):
     produtos = Produto.objects.filter(disponivel=True)
-    return render(request, 'core/index.html')
+    return render(request, 'core/index.html', {'produtos': produtos})
+
+def edit(request):
+    produtos = Produto.objects.filter(disponivel=True)
+    return render(request, 'core/login.html', {'produtos': produtos})
